f8b6c89651660c0d8dbdf0b6987c85961620ff0a
john <johnny.lioup@gmail.com>
2022-10-06 10:58:38 +0100

    added some comments and refactored code for parameters to be more easily set

--- photo_reduce_size.py
+++ photo_reduce_size.py
@@ -4,6 +4,32 @@ from PIL import Image
 inDirectory = r'C:/photos_in'
 outDirectory = r'C:/photos_out'
 
+# New Image parameters
+newImageSize = 1500, 1500
+newImageQuality = 75
+
+# New Image settings
+optimizeImage = True
+resize = True
+
+# New Image output type
+outputType = ".jpg"
+
+def resizeImage(image, newSize):
+    # downsize the image with an ANTIALIAS filter (gives the highest quality)
+    return image.resize(newSize, Image.Resampling.LANCZOS)
+
+def convertImage(image):
+    # If it is stored with transparence we need to remove the transparency as jpg does not support it
+    if image.mode in ("RGBA", "P"):
+        return image.convert("RGB")
+    else:
+        return image
+
+def exportImage(image):
+    image.save(outDirectory + '/' + fileNameClean + outputType, optimize=optimizeImage, quality=newImageQuality)
+    
+
 print("Files need to be in the directory: " + inDirectory)
 print("Output will be generated in the directory: " + outDirectory)
 
@@ -13,17 +39,30 @@ for subdir, dirs, files in os.walk(inDirectory):
         print("Converting file with name: " + filename)
         filepath = subdir + os.sep + filename
         fileNameClean = filename.rsplit('.', 1)[0]
+        fileExtension = filename.rsplit('.', 1)[1]
         if filepath.endswith(".png"):
             try:
                 im = Image.open(filepath)
-                if im.mode in ("RGBA", "P"): im = im.convert("RGB")
+                
+                if resize:             
+                    im = resizeImage(im, newImageSize)
+                
+                if outputType == ".jpg":
+                    im = convertImage(im)
 
-                # downsize the image with an ANTIALIAS filter (gives the highest quality)
-                im = im.resize((1500,1500),Image.ANTIALIAS)
-                im.convert('RGB')
-
-                # im.save(outDirectory + '/' + filename + '.jpg', quality=75)  # The saved downsized image size is 24.8kb
-
-                im.save(outDirectory + '/' + fileNameClean + '.jpg', optimize=True, quality=75)  # The saved downsized image size is 22.9kb
+                exportImage(im)
             except Exception as e: 
                 print("Error! Try again. Error: " + str(e))
+        elif filepath.endswith(".jpg"):
+            try:
+                im = Image.open(filepath)
+
+                if resize:             
+                    resizeImage(im, newImageSize)
+                
+                exportImage(im)
+
+            except Exception as e: 
+                print("Error! Try again. Error: " + str(e))
+        else:
+            print("File Type not supported! For file with name: " + fileNameClean + " and file extension: " + fileExtension)
